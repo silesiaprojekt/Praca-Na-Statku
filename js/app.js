16a5e2e7bd077d3b7cb6506a828292d79adee0ed
platformarobocza <platformarobocza@gmail.com>
2017-03-10 23:29:35 +0100

    New project

--- js/app.js
+++ js/app.js
@@ -1,105 +1,14 @@
-$(document).ready(function(){
+var lat = 59.36572;
+var lon = 10.62675;
 
-
-     $(".buttons .btn").click(function(){
-
-         var $element = $(this).data("id");
-
-         $("#" + $element).prev("div").hide();
-         $("#" + $element).next("div").hide();
-         $("#" + $element).fadeIn();
-
-
-    });
-
-
-    $(".buttons .btn").click(function(){
-
-        $(this).addClass("active");
-
-});
-
-
-    $("#mail p").click(function(){
-        $(this).next("p").fadeToggle();
-    });
-    $("#new p").click(function(){
-        $(this).next("p").fadeToggle();
-    });
-
-
-     $(".panel .btn").click(function(){
-
-         $(".panel .btn").first().addClass("active");
-         var $element = $(this).data("id");
-         $(this).addClass("active").prev("div .btn").removeClass("in-active");
-
-         $("#" + $element).prevAll("div").hide();
-         $("#" + $element).nextAll("div").hide();
-         $("#" + $element).fadeIn();
-
-    });
-
-
-
-     $(".opt").addClass("not-display");
-    $("#opt").click(function(){
-       $(".opt").fadeToggle("display-it");
-    });
-
-    $(".help").addClass("not-display");
-    $("#help").click(function(){
-       $(".help").fadeToggle("display-it");
-    });
-
-
-
-    $("#log-off").click(function(){
-        alert("To jest wersja demo, nie można się wylogować!")
-    })
-
-
-
-
-
-
-    $(".help i").click(function(){
-        $(".help").hide();
-    });
-
-    $(".opt i").click(function(){
-        $(".opt").hide();
-    });
-
-
-    $(".finanse").addClass("not-display");
-    $(".certyfikaty").addClass("not-display");
-    $(".armatorzy").addClass("not-display");
-
-    $(".main li").click(function(){
-
-
-         var $element = $(this).data("id");
-        $("#" + $element ).addClass("display-it")
-
-
-    });
-
-    /*
-
-    $("#new").addClass("display-it");
-
-    $(".button .btn").click(function(){
-
-        var id = $(this).data("id");
-
-        $("#"+id).removeClass("display-it");
-
-    })
-
-
-
-    $("#new > p").click(function(){
-        $(this).next("p").toggleClass("display-it");
-    });*/
-});
+function initMap() {
+        var uluru = {lat: lat, lng: lon};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
